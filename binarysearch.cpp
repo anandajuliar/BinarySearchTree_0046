341162b680cad3f541f3e8c76946bf93c097c68d
anandajuliar <anandajulia001@gmail.com>
2024-06-13 18:47:01 +0700

    else if binarytree

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -50,7 +50,10 @@ public:
         {
             parent->leftchild = newNode; // Make the left child of the parent point to the new node
         }
-       
+        else if(element > parent->info) // If the value in the data field of the new node is greater than that of the
+        {
+            parent->rightchild = newNode; // Make the right child of the parent point to the new node
+        }
     }
 
 
